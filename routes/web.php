869ccf269ebadd3416f69b393f7782c166935621
DeogratiasAmas <deogratiasprog@gmail.com>
2025-01-14 16:49:58 +0300

    Added resource task controller, route and requests for store and update methods

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 use App\Enums\PermissionEnum;
 
@@ -20,6 +21,7 @@ Route::middleware('auth')->group(function () {
     Route::resource("users", UserController::class)->middleware('can:'.PermissionEnum::MANAGE_USERS->value);
     Route::resource("clients", ClientController::class);
     Route::resource("projects", ProjectController::class);
+    Route::resource("tasks", TaskController::class);
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
